af4201b6e8480aa95e26443cd127ceb06317e9a0
Vignesh K K <kkvignesh2000@gmail.com>
2024-09-11 13:56:07 +0530

    Added UI Routes / Components

---
+++ frontend/src/components/Home/CreatePost.tsx
@@ -0,0 +1,90 @@
+import { CiImageOn } from "react-icons/ci";
+import { BsEmojiSmileFill } from "react-icons/bs";
+import { useRef, useState } from "react";
+import { IoCloseSharp } from "react-icons/io5";
+
+const CreatePost = () => {
+  const [text, setText] = useState("");
+  const [img, setImg] = useState<any>();
+
+  const imgRef = useRef<HTMLInputElement>(null);
+
+  const isPending = false;
+  const isError = false;
+
+  const data = {
+    profileImg: "/avatars/boy1.png",
+  };
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    alert("Post created successfully");
+  };
+
+  const handleImgChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (!e.target.files) return;
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (reader.result) setImg(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div className="flex p-4 items-start gap-4 border-b border-gray-700">
+      <div className="avatar">
+        <div className="w-8 rounded-full">
+          <img src={data.profileImg || "/avatar-placeholder.png"} />
+        </div>
+      </div>
+      <form className="flex flex-col gap-2 w-full" onSubmit={handleSubmit}>
+        <textarea
+          className="textarea w-full p-0 text-lg resize-none border-none focus:outline-none  border-gray-800"
+          placeholder="What is happening?!"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        {img && (
+          <div className="relative w-72 mx-auto">
+            <IoCloseSharp
+              className="absolute top-0 right-0 text-white bg-gray-800 rounded-full w-5 h-5 cursor-pointer"
+              onClick={() => {
+                setImg(undefined);
+                if (imgRef.current) imgRef.current.value = "";
+              }}
+            />
+            <img
+              src={img}
+              className="w-full mx-auto h-72 object-contain rounded"
+            />
+          </div>
+        )}
+
+        <div className="flex justify-between border-t py-2 border-t-gray-700">
+          <div className="flex gap-1 items-center">
+            <CiImageOn
+              className="fill-primary w-6 h-6 cursor-pointer"
+              onClick={() => imgRef.current?.click()}
+            />
+            <BsEmojiSmileFill className="fill-primary w-5 h-5 cursor-pointer" />
+          </div>
+          <input
+            type="file"
+            accept="image/*"
+            hidden
+            ref={imgRef}
+            onChange={handleImgChange}
+          />
+          <button className="btn btn-primary rounded-full btn-sm text-white px-4">
+            {isPending ? "Posting..." : "Post"}
+          </button>
+        </div>
+        {isError && <div className="text-red-500">Something went wrong</div>}
+      </form>
+    </div>
+  );
+};
+export default CreatePost;
