5b15a1f0437ab25249e13f8aef441c59b3952abc
Vignesh K K <kkvignesh2000@gmail.com>
2024-09-11 14:08:07 +0530

    fixed typo in api error handler

--- backend/middlewares/errorHandler.ts
+++ backend/middlewares/errorHandler.ts
@@ -5,30 +5,32 @@ const errorHandlerMiddleware: ErrorRequestHandler = (err, req, res, next) => {
   let customError = {
     // set default
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: err.message || "Something went wrong try again later",
+    message: err.message || "Something went wrong try again later",
   };
 
   if (err.name === "ValidationError") {
-    customError.msg = Object.values(err.errors)
+    customError.message = Object.values(err.errors)
       .map((item: any) => item.message)
       .join(",");
     customError.statusCode = StatusCodes.BAD_REQUEST;
   }
 
   if (err.code && err.code === 11000) {
-    customError.msg = `Duplicate value entered for ${Object.keys(
+    customError.message = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`;
     customError.statusCode = StatusCodes.BAD_REQUEST;
   }
 
   if (err.name === "CastError") {
-    customError.msg = `No item found with id : ${err.value}`;
+    customError.message = `No item found with id : ${err.value}`;
     customError.statusCode = StatusCodes.NOT_FOUND;
   }
 
   console.log(`Error: `, err);
-  return res.status(customError.statusCode).json({ msg: customError.msg });
+  return res
+    .status(customError.statusCode)
+    .json({ error: customError.message });
 };
 
 export default errorHandlerMiddleware;
